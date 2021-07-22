e37617c1418c097590a686fd7c17e6b1da2bb79a
Josue Lobo <darksiders20005@gmail.com>
2021-07-22 19:30:30 -0300

    css ok

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,8 +12,8 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
-          <Rating className="movie-card-rating" rating={ rating } />
         </div>
+        <Rating rating={ rating } />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,8 +5,10 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className="rating">
-        { rating }
+      <div className="movie-card-rating">
+        <div className="rating">
+          { rating }
+        </div>
       </div>
     );
   }
