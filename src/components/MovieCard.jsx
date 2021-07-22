28abf3a76864ed01365617ad91a59972830fb8e6
Josue Lobo <darksiders20005@gmail.com>
2021-07-22 16:18:12 -0300

    2 ao 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    const cards = movies.map((mov) => <MovieCard key={ mov.title } movie={ mov } />);
+
+    return (
+      <main>
+        { cards }
+      </main>
+    );
+  }
+}
+
+export default MovieList;
